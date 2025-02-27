ccf9c8fd2131a630528be9a6f777a23bf769a4c3
kouheigit <ookawakouhei777@gmail.com>
2025-02-28 00:17:35 +0900

    feat:バリデーション実装

--- src/app/Http/Requests/TestValidationRequest.php
+++ src/app/Http/Requests/TestValidationRequest.php
@@ -22,12 +22,23 @@ class TestValidationRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'name'=>['required','string','max:20'],
+            'title'=>['required','string','max:30'],
             'comment'=>['required','string','max:300'],
+
         ];
     }
     public function messages(): array
     {
         return [
+            'name.require'=>'名前は入力してください',
+            'name.string'=>'名前は文字列で入力してください',
+            'name.max'=>'名前は20文字以内にしてください',
+
+            'title.require'=>'コメントを入力してください',
+            'title.string'=>'コメントは文字列で入力してください',
+            'title.max'=>'タイトルは30文字以内にしてください',
+
             'comment.require'=>'コメントを入力してください',
             'commnet.string'=>'コメントは文字列で入力してください',
             'comment.max'=>'コメントは300文字以内にしてください',
