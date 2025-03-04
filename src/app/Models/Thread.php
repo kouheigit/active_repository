2fb51925af8bf2cffa05d2d9ec8dd53367749df3
kouheigit <ookawakouhei777@gmail.com>
2025-03-04 21:34:24 +0900

    fix:index.blade.phpの修正

--- src/app/Models/Thread.php
+++ src/app/Models/Thread.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','title', 'comment','$image_title'];
+    protected $fillable = ['name','title', 'comment','fileName'];
 }
