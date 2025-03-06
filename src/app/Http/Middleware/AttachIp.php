9a0feeaf6fcd5f8ad42cea570a32d5c6dd23bc4d
kouheigit <ookawakouhei777@gmail.com>
2025-03-06 22:47:56 +0900

    feat:AttachMiddrewareの実装

--- src/app/Http/Middleware/AttachIp.php
+++ src/app/Http/Middleware/AttachIp.php
@@ -15,8 +15,11 @@ class AttachIp
      */
     public function handle(Request $request, Closure $next): Response
     {
+        //ユーザーのIPを取得する
         $ip = $_SERVER['REMOTE_ADDR'];
+        //スマホかPCかを判定するために、ユーザーエージェントを確認：
         $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        //ユーザーのIPアドレスが「モバイル回線」なのか「固定回線」なのかを判定する方法として、IPアドレスの範囲やホスト名をチェック
         $host = gethostbyaddr($ip);
 
         return $next($request);
