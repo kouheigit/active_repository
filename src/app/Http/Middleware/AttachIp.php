ce73694e021f139c0c971a6e604ef1a287334227
kouheigit <ookawakouhei777@gmail.com>
2025-03-12 18:10:43 +0900

    feat:Middrewareの追加実装

--- src/app/Http/Middleware/AttachIp.php
+++ src/app/Http/Middleware/AttachIp.php
@@ -13,19 +13,45 @@ class AttachIp
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
+    //後で消す
+    function getUserIp()
+    {
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ipList = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            foreach ($ipList as $ip) {
+                $ip = trim($ip);
+                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+                    return $ip;
+                }
+            }
+        }
+
+        if (!empty($_SERVER['HTTP_CLIENT_IP']) && filter_var($_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        }
+
+        return $_SERVER['REMOTE_ADDR'];
+    }
+
+    public function getUserIpp()
+    {
+        if(!empty($SERVER['HTTP_X_FORWARDED_FOR'])){
+            $ipList = explode();
+        }
+    }
     public function handle(Request $request, Closure $next): Response
     {
-        //ユーザーのIPを取得する
+        /*
+        ユーザーのIPを取得する
         $ip = $_SERVER['REMOTE_ADDR'];
-        //スマホかPCかを判定するために、ユーザーエージェントを確認
+        スマホかPCかを判定するために、ユーザーエージェントを確認
         $userAgent = $_SERVER['HTTP_USER_AGENT'];
-        //ユーザーのIPアドレスが「モバイル回線」なのか「固定回線」なのかを判定する方法として、IPアドレスの範囲やホスト名をチェック
-        $host = gethostbyaddr($ip);
+        ユーザーのIPアドレスが「モバイル回線」なのか「固定回線」なのかを判定する方法として、IPアドレスの範囲やホスト名をチェック
+        */
+        //$host = gethostbyaddr($ip);
 
         $userAgent = $_SERVER['HTTP_USER_AGENT'];
-
-
-
+        
         //middlewareからControllerへの値の受け渡し
         $test_value =  $userAgent;
         $request->merge(['test_value' => $test_value]);
