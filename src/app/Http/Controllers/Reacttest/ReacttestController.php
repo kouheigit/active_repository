a21e71ea4a04355f6ecd6a6bad1e6fee3826880e
kouheigit <ookawakouhei777@gmail.com>
2025-03-30 19:38:33 +0900

    feat:ReacttestControllerの導入

--- src/app/Http/Controllers/Reacttest/ReacttestController.php
+++ src/app/Http/Controllers/Reacttest/ReacttestController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers\Reacttest;
 
 use App\Http\Controllers\Controller;
-use http\Env\Request;
+use Illuminate\Http\Request;
 
-class Reacttest extends Controller
+class ReacttestController extends Controller
 {
     public function index(Request $request)
     {
         return view('reacttest.index');
-       // return view('test.index',compact('threads','order'));
     }
 }
