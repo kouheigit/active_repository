83c64b965e26905ac1b4789d19b96939e514ae32
kouheigit <ookawakouhei777@gmail.com>
2025-03-31 23:23:45 +0900

    feat: react index.blade.phpの実装

--- src/resources/js/components/Counter.jsx
+++ src/resources/js/components/Counter.jsx
@@ -4,6 +4,7 @@ export default function Counter(){
     return (
         <div>
             <h2>カウンターアプリ</h2>
+            <h3>カウンター</h3>
             <p>現在のカウント{count}</p>
             <button onClick={() => setCount(count + 1)}>+1</button>
         </div>
