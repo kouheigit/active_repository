bd7a82116ac4f247807dbbd3e104fba655679da4
kouheigit <ookawakouhei777@gmail.com>
2025-03-31 20:39:21 +0900

    fix:app.js→app.jsxへリファクタリング

---
+++ src/resources/js/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+export default function Counter(){
+    const [ count,setCount] = useState(0)
+    return (
+        <div>
+            <h2>カウンターアプリ</h2>
+            <p>現在のカウント{count}</p>
+            <button onClick={() => setCount(count + 1)}>+1</button>
+        </div>
+    )
+}
+
