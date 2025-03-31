5752ce52afa0f30f9e82828f3a3cb5c69fd794e2
kouheigit <ookawakouhei777@gmail.com>
2025-03-31 23:01:26 +0900

    fix:react index.blade.phpの修正

--- src/resources/js/components/Counter.jsx
+++ src/resources/js/components/Counter.jsx
@@ -4,8 +4,6 @@ export default function Counter(){
     return (
         <div>
             <h2>カウンターアプリ</h2>
-            <h1>テスト</h1>
-            <h1>テスト</h1>
             <p>現在のカウント{count}</p>
             <button onClick={() => setCount(count + 1)}>+1</button>
         </div>
