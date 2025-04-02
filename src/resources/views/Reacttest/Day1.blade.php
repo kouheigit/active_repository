e04068ce34cc404927dfc723c0b2722df322fb43
kouheigit <ookawakouhei777@gmail.com>
2025-04-02 17:43:06 +0900

    feat:Day1.jsxの実装

--- src/resources/views/Reacttest/Day1.blade.php
+++ src/resources/views/Reacttest/Day1.blade.php
@@ -1,5 +1,16 @@
 <h1>Day1</h1>
-
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>増減付カウンター</title>
+</head>
+<body>
+<div id="react-day1root">
+    @vite('resources/js/app.jsx)
+</div>
+</body>
+</html>
 <!--
 <!DOCTYPE html>
 <html lang="ja">
