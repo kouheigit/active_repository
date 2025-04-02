a73841d8a1a04217261fbaa5d5991e4f914281c8
kouheigit <ookawakouhei777@gmail.com>
2025-04-02 09:54:58 +0900

    feat:Counter.jsxの実装

---
+++ src/resources/views/Reacttest/Day1.blade.php
@@ -0,0 +1,15 @@
+<h1>Day1</h1>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>React Day1コード</title>
+</head>
+<body>
+<div id="react-day1">
+    <!--  @vite('resources/js/Day1)-->
+    @vite('resources/js/appday1.jsx)
+</div>
+</body>
+</html>
+
