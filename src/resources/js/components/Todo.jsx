75159c1a367c9a2969af89c2766e2fa275791b10
kouheigit <ookawakouhei777@gmail.com>
2025-04-03 20:53:16 +0900

    feat:Todo.jsxの実装

--- src/resources/js/components/Todo.jsx
+++ src/resources/js/components/Todo.jsx
@@ -3,9 +3,10 @@ export default function Todo(){
     const [todos,setTodos] = useState([]);
     return (
         <div>
-            <h1>アンパン</h1>
+            <h1>テスト表示</h1>
             <input  value={todos} onChange={(e => setTodos(e.target.value))}/>
             <p>あなたの名前は{input}です</p>
         </div>
     )
 }
+
