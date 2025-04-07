000fa860f814f711e662d74db83822490aed6c3f
kouheigit <ookawakouhei777@gmail.com>
2025-04-07 17:38:09 +0900

    feat:Todoの追加実装

--- src/resources/js/components/Todo.jsx
+++ src/resources/js/components/Todo.jsx
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 export default function Todo(){
     const [todos,setTodos] = useState([]);
     const [inputs,setInput] = useState('');
+
     const addTodo = ()=>{
         if(inputs.trim()==='') return;
         setTodos([...todos,inputs]);
         setInput('');
     };
+
     return (
         <div>
             <input type="text" value={inputs} onChange={(e) => setInput(e.target.value)}/>
             <p>入力された値: {inputs}</p>
             <button onClick={addTodo}>追加</button>
+            {todos.map((todo,index)=>(
+                <li key={index}>{todo}</li>
+            ))}
         </div>
     )
 }
