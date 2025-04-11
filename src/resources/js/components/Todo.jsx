d83d19beb233695ecdbbedfae839256a63e13db9
kouheigit <ookawakouhei777@gmail.com>
2025-04-11 20:30:47 +0900

    feat:Todo不要なコードを削除

--- src/resources/js/components/Todo.jsx
+++ src/resources/js/components/Todo.jsx
@@ -24,25 +24,3 @@ export default function Todo(){
     )
 }
 
-/*
-function Todo(){
-    const [todos,setTodos] = useState('');
-    return (
-        <div className="todo">
-            <input
-                type="text"
-                value={todos}
-                onChange={(e) => setTodos(e.target.value)}
-            />
-            <p>入力された値: {todos}</p>
-        </div>
-    );
-}
-
-export default Todo;
-
-if (document.getElementById('todo')) {
-    ReactDOM.render(<Todo />, document.getElementById('todo'));
-}
-
- */
