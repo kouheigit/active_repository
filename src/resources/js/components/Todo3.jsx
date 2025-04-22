1411da292374cf9f07e2a81973f452abbf7de610
kouheigit <ookawakouhei777@gmail.com>
2025-04-22 13:08:47 +0900

    feat:Todo3.jsxの実装

---
+++ src/resources/js/components/Todo3.jsx
@@ -0,0 +1,12 @@
+import { useCounter } from "./hooks/useCounter1";
+import { useState,useEffect  } from "react";
+
+export default function Todo3(){
+    //配列を格納する
+    const [todos,setTodos] = useState([]);
+    const[inputs,setInput] = useState('');
+    /*
+    return(
+
+    );*/
+}
