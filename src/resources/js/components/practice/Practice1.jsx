c1060b3bba7595861977fe5488282d0e396fee6b
kouheigit <ookawakouhei777@gmail.com>
2025-05-09 21:43:12 +0900

    feat:practice1.jsxの実装

--- src/resources/js/components/practice/Practice1.jsx
+++ src/resources/js/components/practice/Practice1.jsx
@@ -1,10 +1,19 @@
 import React,{ useState,useRef } from 'react';
 export default function Practice1(){
     const inputRef = useRef(null);
+    const inputRef1 = useRef(null);
+
+    const handleforcus = () =>{
+        inputRef1.current.focus();
+    }
+
     return (
         <div>
+            <b>inputRef</b>
             <input ref={inputRef} />
             <button onClick={() => inputRef.current.focus()}>フォーカスする</button>
+            <b>inputRef1</b>
+
         </div>
     )
 }
