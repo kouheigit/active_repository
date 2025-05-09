6d3210c1f2301d3cb1e228ed8479e26de33b3658
kouheigit <ookawakouhei777@gmail.com>
2025-05-09 18:38:02 +0900

    feat:react-practice.blade.phpの実装

---
+++ src/resources/views/Reacttest/reactpractice.blade.php
@@ -0,0 +1,13 @@
+<h1>TODOリスト</h1>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>React-practice</title>
+</head>
+<body>
+<div id="react-practice">
+    @vite('resources/js/app.jsx')
+</div>
+</body>
+</html>
